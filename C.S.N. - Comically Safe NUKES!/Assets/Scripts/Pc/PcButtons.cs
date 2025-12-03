188f6398f25de945e093af87cc7037e69c61462f
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-12-03 10:44:07 +0000

    Pc puzzle functional

--- C.S.N. - Comically Safe NUKES!/Assets/Scripts/Pc/PcButtons.cs
+++ C.S.N. - Comically Safe NUKES!/Assets/Scripts/Pc/PcButtons.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PcButtons : Interactive
 {
     [SerializeField] private GameObject[] targetObjects;
+    [SerializeField] private int numoftrue;
+    [SerializeField] private Interactive microwave;
 
     protected override void InteractSelf(bool direct)
     {
@@ -11,6 +13,25 @@ public class PcButtons : Interactive
             if (obj != null)
             {
                 obj.SetActive(!obj.activeSelf);
+                    foreach (GameObject luz in targetObjects)
+                    {
+                        if (luz.activeSelf == true)
+                        {
+                            numoftrue += 1;
+                        }
+                        else
+                        {
+                            numoftrue = 0; //TESTE PROVAVELMENTE ERRADO 
+                        }
+                    }
+                if (numoftrue == targetObjects.Length)
+                {
+                    microwave.isOn = true;
+                }
+                else
+                {
+                    microwave.isOn = false;
+                }
             }
         }
         base.InteractSelf(direct);
