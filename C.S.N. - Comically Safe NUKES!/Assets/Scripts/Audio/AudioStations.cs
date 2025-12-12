3237ce9a8ba9c6f3b6a79e817d9b56f961e6eba6
Syndher <miguelfilipe.dev@gmail.com>
2025-12-12 22:53:24 +0000

    fix on AudioStation

--- C.S.N. - Comically Safe NUKES!/Assets/Scripts/Audio/AudioStations.cs
+++ C.S.N. - Comically Safe NUKES!/Assets/Scripts/Audio/AudioStations.cs
@@ -42,4 +42,8 @@ public class AudioStations : MonoBehaviour
         if (_playOnEnable)
             PressButton();
     }
+     private AudioClip GetClipFromIndex(int index)
+    {   
+        return _soundClips[index];
+    }
 }
