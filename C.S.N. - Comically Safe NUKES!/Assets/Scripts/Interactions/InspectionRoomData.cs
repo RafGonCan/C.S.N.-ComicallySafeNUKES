488c749796a334a99d6f6c6e828f6adfefd05e74
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-12-17 16:25:18 +0000

    removed unnecessary inspectioncamera parameters

--- C.S.N. - Comically Safe NUKES!/Assets/Scripts/Interactions/InspectionRoomData.cs
+++ C.S.N. - Comically Safe NUKES!/Assets/Scripts/Interactions/InspectionRoomData.cs
@@ -5,16 +5,10 @@ public class InspectionRoomData : MonoBehaviour
     [SerializeField] private Camera _inspectionCamera;
     [SerializeField] private Vector3 _objectPosition = new Vector3(0, 0, 2);
     [SerializeField] private Vector3 _objectRotation = Vector3.zero;
-    [SerializeField] private float _cameraFOV = 60f;
-    [SerializeField] private float _nearClipPlane = 0.1f;
-    [SerializeField] private float _farClipPlane = 10f;
     public Transform InspectionRoom => transform;
     public Camera InspectionCamera => _inspectionCamera;
     public Vector3 ObjectPosition => _objectPosition;
     public Vector3 ObjectRotation => _objectRotation;
-    public float CameraFOV => _cameraFOV;
-    public float NearClipPlane => _nearClipPlane;
-    public float FarClipPlane => _farClipPlane;
     
     void Awake()
     {
@@ -25,15 +19,5 @@ public class InspectionRoomData : MonoBehaviour
             _inspectionCamera.enabled = false;
         else
             Debug.LogError("No Camera found as child of InspectionRoom!");
-    }
-    
-    public void SetupCamera()
-    {
-        if (_inspectionCamera != null)
-        {
-            _inspectionCamera.fieldOfView = _cameraFOV;
-            _inspectionCamera.nearClipPlane = _nearClipPlane;
-            _inspectionCamera.farClipPlane = _farClipPlane;
-        }
     }
 }
