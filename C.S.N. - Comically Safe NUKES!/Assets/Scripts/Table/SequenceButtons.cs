780691e3b40b86e0cc7a574ad1e2f9e2c5d96b66
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-12-18 12:51:14 +0000

    Added comments to table puzzle for clearer code

--- C.S.N. - Comically Safe NUKES!/Assets/Scripts/Table/SequenceButtons.cs
+++ C.S.N. - Comically Safe NUKES!/Assets/Scripts/Table/SequenceButtons.cs
@@ -5,25 +5,32 @@ public class SequenceButton : Interactive
     [SerializeField] private TableController tableController;
     [SerializeField] private int buttonIndex;
     private bool _isPressed;
-
+    /// <summary>
+    /// Overrides the InteractSelf function by adding a checker feature for the buttons.
+    /// Checking if they available to be pressed.
+    /// </summary>
+    /// <param name="direct"></param>
     protected override void InteractSelf(bool direct)
     {
-        // Verifica se dá para clicar
+        // Checks if the button is able to be pressed
         if (_isPressed || (tableController != null && !tableController.CanPressButton(buttonIndex)))
         {
             return;
         }
-    
+
+        // If the button can be pressed, it will call the base function    
         base.InteractSelf(direct);
         
-        // Clica e notifica
+        // Then it records it
         _isPressed = true;
         if (tableController != null)
         {
             tableController.OnButtonPressed(buttonIndex);
         }
     }
-    
+    /// <summary>
+    /// Simple button reset function
+    /// </summary>
     public void ResetButton()
     {
         _isPressed = false;
--- C.S.N. - Comically Safe NUKES!/Assets/Scripts/Table/TableController.cs
+++ C.S.N. - Comically Safe NUKES!/Assets/Scripts/Table/TableController.cs
@@ -10,16 +10,23 @@ public class TableController : MonoBehaviour
     [SerializeField] private AudioClip _wrongSequence;
     [SerializeField] private Animator _panelAnimator;
     
-    private List<int> currentSequence = new List<int>(); //sequencia que está a ser introduzida
+    private List<int> currentSequence = new List<int>(); //Introduced sequence
     private bool _isSolved = false;
-    private List<int> pressedButtons = new List<int>(); // lista de botões clicados
+    private List<int> pressedButtons = new List<int>(); //Buttons that have been added to the sequence
     
     public bool IsSolved => _isSolved;
 
     void Start()
     {
+        //Sets every button to "on", if we want to add requirements to turning on the puzzle
+        // We just need to call this function at the end
         SetButtonState(true);
+        
     }
+    /// <summary>
+    /// Sets every button state to true/false
+    /// </summary>
+    /// <param name="state"></param>
     private void SetButtonState(bool state)
     {
         foreach (GameObject button in buttons)
@@ -27,7 +34,12 @@ public class TableController : MonoBehaviour
             if(button != null) button.SetActive(state);
         }
     }
-
+    /// <summary>
+    /// If the puzzle is solved or that button has been pressed, you can't press the button again until finishing a sequence.
+    /// However, everytime a button is pressed, it is added to the sequence.
+    /// Everytime it is added to the sequence, CheckSequence() is called to see if it is correct.
+    /// </summary>
+    /// <param name="buttonIndex"></param>
     public void OnButtonPressed(int buttonIndex)
     {
         if (_isSolved) return;
@@ -44,6 +56,10 @@ public class TableController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Compares the correct sequence to the current sequence and sets "correct" to true/false
+    /// if "correct" is true, it calls PuzzleSolved(), otherwise PuzzleFailed();
+    /// </summary>
     private void CheckSequence()
     {
         bool correct = true;
@@ -55,8 +71,7 @@ public class TableController : MonoBehaviour
                 correct = false;
                 break;
             }
-        }
-        
+        }        
         if (correct)
         {
             PuzzleSolved();
@@ -66,21 +81,28 @@ public class TableController : MonoBehaviour
             PuzzleFailed();
         }
     }
-
+    /// <summary>
+    /// When the sequence is correct, it calls the animator's boolean "solved" and "_isSolved", setting both to true
+    /// Playing the compartment opening animation
+    /// </summary>
     private void PuzzleSolved()
     {
         _isSolved = true;
-        Debug.Log("Tá certo");
         _panelAnimator.SetBool("Solved", true);
         AudioSource.PlayClipAtPoint(_rightSequence, transform.position);
     }
-
+    /// <summary>
+    /// When the sequence is incorrect, it resets the puzzle by calling ResetPuzzle() after 0.5 seconds
+    /// </summary>
     private void PuzzleFailed()
     {
         Debug.Log("Tá errado");
         Invoke(nameof(ResetPuzzle), 0.5f);
     } 
-
+    /// <summary>
+    /// Resets te puzzle by clearing the pressed buttons and the current sequence
+    /// Also plays the sound for when doing the wrong sequence
+    /// </summary>
     private void ResetPuzzle()
     {
         Debug.Log("Resetting");
@@ -89,7 +111,7 @@ public class TableController : MonoBehaviour
         
         foreach (var button in buttons)
         {
-            var seqButton = button.GetComponent<SequenceButton>();
+            SequenceButton seqButton = button.GetComponent<SequenceButton>();
             if (seqButton != null)
             {
                 seqButton.ResetButton();
@@ -97,6 +119,12 @@ public class TableController : MonoBehaviour
         }
         AudioSource.PlayClipAtPoint(_wrongSequence, transform.position);
     }
+    /// <summary>
+    /// Simple boolean checker for when you can or can't press a button
+    /// You have to finish a sequence to re-press a button
+    /// </summary>
+    /// <param name="buttonIndex"></param>
+    /// <returns></returns>
     public bool CanPressButton(int buttonIndex)
     {
         return !_isSolved && !pressedButtons.Contains(buttonIndex);
