1411888ae8633966e28954e2b878c3c92a0044ff
Diogo <diogofonseca.gamedesigner@gmail.com>
2026-07-13 10:31:50 +0100

    SubtitleManager Singleton

--- C.S.N. - Comically Safe NUKES!/Assets/Scripts/Audio/SubtitleManager.cs
+++ C.S.N. - Comically Safe NUKES!/Assets/Scripts/Audio/SubtitleManager.cs
@@ -1,16 +1,27 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SubtitleManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SubtitleManager Instance;
+
+    [SerializeField] private TMP_Text subtitleText;
+
+    [SerializeField] private List<string> _trackKeys = new List<string>();
+
+    [SerializeField, TextArea(3, 15)] private List<string> _trackData = new List<string>();
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
