e406a72d11852537dccc22b09a862152549cf15c
Miguel Filipe <miguelfilipe.dev@gmail.com>
2026-07-17 23:05:18 +0100

    fixed pipe puzzle

--- C.S.N. - Comically Safe NUKES!/Assets/Scripts/Pc/PcButtons.cs
+++ C.S.N. - Comically Safe NUKES!/Assets/Scripts/Pc/PcButtons.cs
@@ -3,37 +3,21 @@ using UnityEngine;
 public class PcButtons : Interactive
 {
     [SerializeField] private GameObject[] targetObjects;
-    [SerializeField] private int numoftrue;
+    [SerializeField] private GameObject[] allPipes;
     [SerializeField] private Interactive microwave;
 
     protected override void InteractSelf(bool direct)
     {
         foreach (GameObject obj in targetObjects)
-        {
-            if (obj != null)
-            {
-                obj.SetActive(!obj.activeSelf);
-                    foreach (GameObject luz in targetObjects)
-                    {
-                        if (luz.activeSelf == true)
-                        {
-                            numoftrue += 1;
-                        }
-                        else
-                        {
-                            numoftrue = 0;
-                        }
-                    }
-                if (numoftrue == targetObjects.Length)
-                {
-                    microwave.isOn = true;
-                }
-                else
-                {
-                    microwave.isOn = false;
-                }
-            }
-        }
+            if (obj != null) obj.SetActive(!obj.activeSelf);
+
+        int activeCount = 0;
+        foreach (GameObject obj in allPipes)
+            if (obj != null && obj.activeSelf) activeCount++;
+
+        if (microwave != null)
+            microwave.isOn = (activeCount == allPipes.Length);
+
         base.InteractSelf(direct);
     }
 }
